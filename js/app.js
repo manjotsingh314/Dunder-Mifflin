a3345eb609b65628af11b17ed63ea666d4908d4c
Manjot Singh <manjotsinghsahni1132@gmail.com>
2026-07-13 23:03:57 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('Dashboard loaded');
+if(localStorage.getItem("isLoggedIn") !== "true"){
+  window.location.href = "index.html";
+}
+
+document.getElementById("logoutBtn")
+.addEventListener("click",function(){
+
+  localStorage.removeItem("isLoggedIn");
+  window.location.href = "index.html";
+
+});
+
+document.getElementById("employeeCount")
+.innerText = 17;
+
+document.getElementById("presentCount")
+.innerText = 15;
+
+document.getElementById("leaveCount")
+.innerText = 2;
+
+document.getElementById("payrollCount")
+.innerText = "$120,000";
